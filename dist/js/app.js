ba4d2e24e82bf0700674f85e4cfddd8f0b6f2991
Gawrun <gawrun.cub@gmail.com>
2024-04-23 14:34:12 +0200

    Dodan jos jedan motivacijski cit at u app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 window.$ = require('./jQuery.js');
-var quotes = ["Ostvari svoj potencijal!", "Nema problema koji ne možete riješiti!", "Svaki dan je nova prilika za uspjeh!", "Nikada nemojte odustati od svojih snova!", "Vi ste izvanredna osoba!"];
+var quotes = ["Ostvari svoj potencijal!", "Nema problema koji ne možete riješiti!", "Svaki dan je nova prilika za uspjeh!", "Nikada nemojte odustati od svojih snova!", "Vi ste izvanredna osoba!", "Git moze biti super stvar, osim ako obrises sve foldere"];
 $(function () {
   var quote = $("#quote");
   var generateBtn = $("#generate");
